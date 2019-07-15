4250ceae1dca88623238dd7b44cfa416809e2f59
Arcangelo Massari <arcangelomas@gmail.com>
2019-07-15 16:29:54 +0200

    Tutto unito

---
+++ config/passport.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require('passport-local').Strategy;
+require("./dbconnection.js").open();
+var dbconn = require("./dbconnection.js");
+
+module.exports = function(passport){
+    passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done)=>{
+    // By default, LocalStrategy expects to find credentials in parameters named username and password. I want an email
+            var db = dbconn.get();
+            var collection = db.collection("utenti");
+            // VERIFICA MAIL
+            collection.findOne({email: email}).then(utente => {
+            if(!utente){
+                return done(null, false, {message: 'Utente non trovato'});
+                // parametri = errori del server, non dell'autenticazione
+                //             false indicates an authentication failure
+            }
+            // VERIFICA PASSWORD
+            if(password == utente.password){
+                return done(null, utente);
+            } else {
+                return done(null, false, {message: 'Password non corretta'});
+            }
+        });
+    }));
+
+    // The user (you provide as the second argument of the done function) is saved in the session and is later used to retrieve th
+    // When subsequent requests are received, this ID is used to find the user, which will be restored to req.user.
+    passport.serializeUser(function(utente, done) {
+        done(null, utente._id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        var db = dbconn.get();
+        var collection = db.collection("utenti");
+        collection.find({_id: id}, function(err, utente) {
+            done(err, utente);
+        });
+    });
+
+}
---
+++ public/script/audio-capture.js
@@ -0,0 +1,84 @@
+map.on('popupopen', function() {
+
+    var gumStream;
+    var recorder;
+    var input;
+    var encodingType;
+    var encodeAfterRecord = true;
+
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
+    var audioContext; //new audio context to help us record
+    
+    var start = document.getElementById('registratore');
+
+    start.addEventListener('click', (ev)=>{
+        console.log("CLICCATO");
+        var constraints = { audio: true, video:false }
+
+        navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+            console.log("then");
+            audioContext = new AudioContext();
+            gumStream = stream;
+            input = audioContext.createMediaStreamSource(stream);
+            encodingType = "mp3";
+
+            
+
+		    recorder = new WebAudioRecorder(input, {
+                workerDir: "script/WebAudioRecorder/", // must end with slash
+                encoding: encodingType,
+                numChannels:2, //2 is the default, mp3 encoding supports only 2
+                onEncoderLoading: function(recorder, encoding) {
+                    // show "loading encoder..." display
+                    console.log("Loading "+encoding+" encoder...");
+                },
+                onEncoderLoaded: function(recorder, encoding) {
+                    // hide "loading encoder..." display
+                    console.log(encoding+" encoder loaded");
+                }
+            });
+
+            recorder.onComplete = function(recorder, blob) { 
+                console.log("Encoding complete");
+                var reader = new FileReader();
+                reader.onload = function () {
+                    b64 = reader.result.replace(/^data:.+;base64,/, '');
+                    document.getElementById("audio").value = b64;
+                    console.log(b64);
+                };
+                reader.readAsDataURL(blob);
+            }
+    
+            recorder.setOptions({
+                timeLimit:120,
+                encodeAfterRecord:encodeAfterRecord,
+                ogg: {quality: 0.5},
+                mp3: {bitRate: 160}
+            });
+
+            //start the recording process
+		    recorder.startRecording();
+
+            console.log("Recording started");
+            
+            let stop = document.getElementById('stop');
+
+            stop.addEventListener('click', (ev)=>{
+                console.log("stopRecording() called");
+	
+                //stop microphone access
+                gumStream.getAudioTracks()[0].stop();
+                //tell the recorder to finish the recording (stop recording + encode the recorded audio)
+                recorder.finishRecording();
+                console.log('Recording stopped');
+            });
+
+    
+        }).catch(function(err) {
+            console.log("catchato");
+        });
+
+
+    });
+
+});
